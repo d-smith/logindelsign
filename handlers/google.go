bff90c36d52ae8555586b7b6b5bb4aa5adf5781f
d-smith <doug.smith.mail@gmail.com>
2023-09-27 11:16:58 -0700

    Initial commit

---
+++ handlers/google.go
@@ -0,0 +1,154 @@
+package handlers
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+// Scopes: OAuth 2.0 scopes provide a way to limit the amount of access that is granted to an access token.
+var googleOauthConfig = &oauth2.Config{
+	RedirectURL:  "http://localhost:3000/auth/google/callback",
+	ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func GenerateToken(ttl time.Duration, payload interface{}, secretJWTKey string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	now := time.Now().UTC()
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["sub"] = payload
+	claims["exp"] = now.Add(ttl).Unix()
+	claims["iat"] = now.Unix()
+	claims["nbf"] = now.Unix()
+
+	tokenString, err := token.SignedString([]byte(secretJWTKey))
+
+	if err != nil {
+		return "", fmt.Errorf("generating JWT Token failed: %w", err)
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(token string, signedJWTKey string) (interface{}, error) {
+	tok, err := jwt.Parse(token, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", jwtToken.Header["alg"])
+		}
+
+		return []byte(signedJWTKey), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("invalidate token: %w", err)
+	}
+
+	claims, ok := tok.Claims.(jwt.MapClaims)
+	if !ok || !tok.Valid {
+		return nil, fmt.Errorf("invalid token claim")
+	}
+
+	return claims["sub"], nil
+}
+func oauthGoogleLogin(w http.ResponseWriter, r *http.Request) {
+
+	// Create oauthState cookie
+	oauthState := generateStateOauthCookie(w)
+
+	/*
+		AuthCodeURL receive state that is a token to protect the user from CSRF attacks. You must always provide a non-empty string and
+		validate that it matches the the state query parameter on your redirect callback.
+	*/
+	u := googleOauthConfig.AuthCodeURL(oauthState)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
+}
+
+func oauthGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	// Read oauthState from Cookie
+	oauthState, _ := r.Cookie("oauthstate")
+
+	if r.FormValue("state") != oauthState.Value {
+		log.Println("invalid oauth google state")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getUserDataFromGoogle(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// GetOrCreate User in your db.
+	// Redirect or response with a token.
+	// More code .....
+	fmt.Fprintf(w, "UserInfo: %s\n", data)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(20 * time.Minute)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
+}
+
+type ClientID struct {
+	Id string `json:"id"`
+}
+
+func getUserDataFromGoogle(code string) ([]byte, error) {
+	// Use code to get token and get user info from Google.
+
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
+	}
+	fmt.Printf("token: %s\n", token.AccessToken)
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+	fmt.Printf("response: %s\n", contents)
+
+	var clientID ClientID
+	err = json.Unmarshal(contents, &clientID)
+	if err != nil {
+		return nil, fmt.Errorf("failed unmarshal response: %s", err.Error())
+	}
+
+	jwtToken, err := GenerateToken(20*time.Minute, clientID.Id, "secret")
+	if err != nil {
+		return nil, fmt.Errorf("failed generate token: %s", err.Error())
+	}
+	fmt.Printf("jwtToken: %s\n", jwtToken)
+
+	return contents, nil
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Login for Delegate Signin Example
+
+Boostrapped from [this project](https://github.com/douglasmakey/oauth2-example/blob/master/templates/index.html)
